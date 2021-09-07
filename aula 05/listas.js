fd0442eccbbd6aef0bb0093beca5ef01ac312a0b
Jhonatan Santiago <jhonssantiago@gmail.com>
2021-09-07 16:19:17 -0300

    aula 5

--- aula 05/listas.js
+++ aula 05/listas.js
@@ -5,89 +5,60 @@ function ajax(caminho,funcao) {
     xhttp.send();
 }
 let doc;
-function iniciarMenu(){
-    if(this.readyState==4&&this.status==200){
+ajax("listas.xml",mostrarTudo);
+
+function mostrarTudo(){
+    if(this.readyState==4&&this.status==200) {
         doc=this.responseXML;
+        let listas=doc.getElementsByTagName("lista");
+        let conteudo = document.getElementById("conteudo");
 
-        let textoItem = "";
-        let listas = doc.getElementsByTagName("lista");
-        let tam = listas.length;
-        let div = document.createElement("div");
-        
-
-        for(let i=0; i<tam; i++){
-            let ul = document.createElement("ul");
-            if(listas[i].nodeType==1){
-                
-                let h3 = document.createElement("h3");
-                let tituloValue = listas[i].getElementsByTagName("titulo")[0].firstChild.nodeValue;
-                let notitulo = document.createTextNode(tituloValue);
-                h3.appendChild(notitulo);
-                div.appendChild(h3);
-
-
-                let item=listas[i].getElementsByTagName("item");
-                let tamitem = item.length;
-                for(let y=0; y< tamitem; y++){
-                    if (item[y].nodeType==1){
-                        
-                        let li=document.createElement("li");
-                        let b = document.createElement("b");
-                        let p = document.createElement("p");
-                        let pri = document.createElement("p");                     
-                        let del = document.createElement("del");
-                        textoItem = item[y].firstChild.nodeValue;
-                        let noItem = document.createTextNode(textoItem);
-                       
-
-                                                  
-                        if(item[y].getAttribute("feito")=="sim"){
-                          //  p.setAttribute("class", "feito");
-                            del.appendChild(noItem);
-
-                            if(item[y].getAttribute("prioridade")=="sim"){
-                                p.setAttribute("class", "prioridade feito");
-                                b.appendChild(del);
-                                p.appendChild(b);
-                            }else{
-                                p.appendChild(del);
-                                p.setAttribute("class", "feito");
-                            }
-                            
-                        } else {
-                          //  p.setAttribute("class", "naofeito");
-                            if(item[y].getAttribute("prioridade")=="sim"){
-                                p.setAttribute("class", "prioridade naofeito");
-                                b.appendChild(noItem);
-                                p.appendChild(b);
-                            }else {
-                                p.appendChild(noItem);
-                                p.setAttribute("class", "naofeito");
-                            }
-                        }          
-                        li.appendChild(p);
-                        ul.appendChild(li);
-                        div.appendChild(ul);
-                    }                 
-                }
-                document.getElementById("titulo").appendChild(div);              
-            }                  
-        }    
+        for(let lista of listas){
+            let div = document.createElement("div");
+			div.setAttribute("class", "lista" );
+			addTitulos(pegaTitulo(lista), div);
+			listaItem(lista, div);
+			conteudo.appendChild(div);
+        }       
     }
 }
 
-/*
-document.getElementById("botao").onclick=()=>{
-    let texto=document.getElementById("pesquisa").value;
-    let titulos=doc.getElementsByTagName("titulo");
-    for (let titulo of titulos) {
-       let pesquisa =  titulo.firstChild.nodeValue;
-       if (texto == pesquisa){
-           console.log("igual");
-       }
-    }
-    
-    
-} */
+function pegaTitulo(lista) {
+	return lista.getElementsByTagName("titulo")[0].firstChild.nodeValue;
+}
 
-ajax("listas.xml",iniciarMenu);
+function listaItem(lista, div) {
+    let itens=lista.getElementsByTagName("item");
+    let ul = document.createElement("ul");
+
+    for (let i = 0; i < itens.length; i++) {
+        if (itens[i].nodeType == 1) {
+			texto = itens[i].firstChild.nodeValue;
+			let feito = document.createElement("del");
+			let b = document.createElement("b");
+            let li = document.createElement("li");
+			let noTxt = document.createTextNode(texto);
+
+			if (itens[i].getAttribute("prioridade") == "sim") {
+				b.appendChild(noTxt);
+				noTxt = b
+			}
+			if (itens[i].getAttribute("feito") == "sim") {
+				feito.appendChild(noTxt);
+                li.setAttribute("class", "feito" );
+				noTxt = feito
+			}
+            li.appendChild(noTxt);
+	        ul.appendChild(li);
+	        div.appendChild(ul);
+		}
+    }
+}
+
+function addTitulos(texto, div) {
+	let h3 = document.createElement("h3");
+	let noTxt = document.createTextNode(texto);
+	h3.appendChild(noTxt);
+	div.appendChild(h3);
+}
+
